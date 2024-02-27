fdc76e44ef23e159eb6590d57afc51d019c48e8e
Mohamed Tarakan <moetarakan@mohameds-air.lan>
2024-02-26 23:27:21 -0500

    created withRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 import './App.css'
+import { Fragment } from 'react'
 
 function App(){
         let [search, setSearch] = useState('')
@@ -23,7 +27,7 @@ function App(){
                             } 
                     }
                     fetchData()
-            }  
+                }  
         }, [search])
 
         const handleSearch = (e, term) => {
@@ -33,9 +37,19 @@ function App(){
 
         return (
             <div>
-                <SearchBar handleSearch={handleSearch} />
-                {message}
-                <Gallery data ={data} />
+            {message}
+                <Router>
+                    <Routes>
+                        <Route path="/" element={
+                            <Fragment>
+                                <SearchBar handleSearch = {handleSearch}/>
+                                <Gallery data={data} />
+                            </Fragment>
+                        } />
+                        <Route path="/album/:id" element={<AlbumView />} />
+                        <Route path="/artist/:id" element={<ArtistView />} />
+                    </Routes>
+                </Router>
             </div>
         )
 }
